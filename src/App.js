ca9466e284c570dd3c2bcd1d49fb5f267c97ad72
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-25 11:30:17 -0800

    Converted anchor tags to use Link Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
 import { Route } from 'react-router-dom';
+import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={Home} />
-    <Route exact path="/contact" component={Home} />
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
   </div>
 );
 
