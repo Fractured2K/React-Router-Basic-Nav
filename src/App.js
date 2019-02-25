6ac83d329e5cdae51338be223d415724d664c45e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-25 11:24:43 -0800

    Created 3 Route Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={Home} />
+    <Route exact path="/contact" component={Home} />
     <Navigation />
   </div>
 );
